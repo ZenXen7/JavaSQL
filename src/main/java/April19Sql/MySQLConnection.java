126051cd0344341dcf05c59cedfca970c7eb61ac
LAB11_USER ON WS25 <L11X09W25@citulab.net>
2024-04-19 08:06:42 +0800

    otin

---
+++ src/main/java/April19Sql/MySQLConnection.java
@@ -0,0 +1,39 @@
+package April19Sql;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class MySQLConnection {
+
+    public static final String URL = "jdbc:mysql://localhost:3306/dboop2";
+
+    public static final String USER = "root";
+
+    public static final String PASS = "";
+
+    public static Connection getConnection(){
+        Connection c = null;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Connection Successful");
+        }catch(ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Connection c = MySQLConnection.getConnection();
+
+        try{
+            c.close();
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
+
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.passwordcracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.example.passwordcracker to javafx.fxml;
+    exports com.example.passwordcracker;
+}
